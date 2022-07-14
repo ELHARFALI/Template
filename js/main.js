3004ddf7114223a162ac7699ee2886d98273cd53
ELHARFALI <elharfali.youssef.20@gmail.com>
2022-07-14 17:02:40 +0100

    navbar with js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 // ------- Navbar -------
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
+// Display Mobile Menu
+const mobieMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobieMenu);
